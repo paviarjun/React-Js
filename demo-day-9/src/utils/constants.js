26b5dcae34390cb1489628d8894c4b9ce74b703b
pavithra <pavithra.murugan@xebia.com>
2024-02-15 21:10:45 +0530

    Updated code

---
+++ demo-day-9/src/utils/constants.js
@@ -0,0 +1,11 @@
+export const LOGO_URL =
+  // "https://cdn.dribbble.com/users/630677/screenshots/3833541/media/201454f743f48c415a38c49419275692.jpg?resize=400x0";
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR08qH6Ajpo3ufTHICbrtNrhFvsSPGA62S-3Q&usqp=CAU"
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660";
+
+export const SWIGGY_URL =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=8.515738899999999&lng=76.90716479999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
+export const SWIGGY_MENU_URL =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=8.515738899999999&lng=76.90716479999999&restaurantId=";
